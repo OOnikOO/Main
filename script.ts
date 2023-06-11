3258c13d61d809777117cd5cbbbf90e952b60724
Александр <yo_yo_87@mail.ru>
2023-06-11 09:46:47 +0300

    Main

--- script.ts
+++ script.ts
@@ -79,10 +79,13 @@ prompt(), а выводить результат с помощью alert().
 
                 //Не понятно, как без if сделать :(
 
-        //let num = +(prompt('Введите целое число.') as string)
+        let num = +(prompt('Введите целое число.') as string)
         //let even = ('Число четное.')
-        //let odd = ('Число нечетное.')
-        //alert(`${even}` || `${odd}`)
+        let odd = ('Число нечетное.')
+        let evenBoolean = num % 2 == 0
+        let oddBoolean = odd
+        let result = evenBoolean || oddBoolean
+        alert(result)
 
 
 
